533c5d4bce1aa87affd662596c9efb672d02e23c
darksv <darek969-12@o2.pl>
2018-06-21 21:21:11 +0200

    Passing source path as a commandline arg

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -1,12 +1,14 @@
 extern crate vm;
 
+use std::env::args;
 use vm::assembly::print_assembly;
 use vm::loader::Loader;
 use vm::interpreter::execute_assembly;
 
 fn main() {
+    let path = args().nth(1).unwrap();
     let mut loader = Loader::new();
-    let asm = loader.load("tests/input.asm");
+    let asm = loader.load(&path);
     print_assembly(&asm);
     execute_assembly(&asm)
 }
